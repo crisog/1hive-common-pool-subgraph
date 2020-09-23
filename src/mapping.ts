c750d27b75995caf4136a845021baf74c2439fb4
Cristopher Ortega <ortega.cpp@gmail.com>
2020-09-23 03:30:03 -0400

    Create subgraph for 1hive common pool

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,69 +1,48 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  Contract,
-  VaultTransfer,
-  VaultDeposit,
-  ScriptResult,
-  RecoverToVault
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+  VaultTransfer as VaultTransferEvent,
+  VaultDeposit as VaultDepositEvent
+} from "../generated/Vault/Vault"
+import { Transfer as TransferEntity, Deposit as DepositEntity } from "../generated/schema"
 
-export function handleVaultTransfer(event: VaultTransfer): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleVaultTransfer(event: VaultTransferEvent): void {
+  
+  let txHash = event.transaction.hash.toHex()
+  let origin = event.transaction.from.toHex()
+  let entity = TransferEntity.load(origin)
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
   if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new TransferEntity(origin)
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity.amount = BigInt.fromI32(0)
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  entity.amount = event.params.amount
 
-  // Entity fields can be set based on event parameters
   entity.token = event.params.token
   entity.to = event.params.to
+  entity.txHash = txHash
 
-  // Entities can be written to the store with `.save()`
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.hasInitialized(...)
-  // - contract.TRANSFER_ROLE(...)
-  // - contract.getEVMScriptExecutor(...)
-  // - contract.getRecoveryVault(...)
-  // - contract.isDepositable(...)
-  // - contract.allowRecoverability(...)
-  // - contract.appId(...)
-  // - contract.getInitializationBlock(...)
-  // - contract.canPerform(...)
-  // - contract.getEVMScriptRegistry(...)
-  // - contract.kernel(...)
-  // - contract.isPetrified(...)
-  // - contract.balance(...)
 }
 
-export function handleVaultDeposit(event: VaultDeposit): void {}
+export function handleVaultDeposit(event: VaultDepositEvent): void {
+  
+  let txHash = event.transaction.hash.toHex()
+  let origin = event.transaction.from.toHex()
+  let entity = DepositEntity.load(origin)
+  
+  if (entity == null) {
+    entity = new DepositEntity(origin)
 
-export function handleScriptResult(event: ScriptResult): void {}
+    entity.amount = BigInt.fromI32(0)
+  }
 
-export function handleRecoverToVault(event: RecoverToVault): void {}
+  entity.amount = event.params.amount
+  entity.token = event.params.token
+  entity.from = event.params.sender
+  entity.txHash = txHash
+
+  entity.save()
+
+}
