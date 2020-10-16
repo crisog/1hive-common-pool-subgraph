fb72203d398f3d5d746c49142d3694f5f6334099
Cristopher Ortega <ortega.cpp@gmail.com>
2020-10-16 18:17:23 -0400

    Added timestamp to the entity

--- src/mapping.ts
+++ src/mapping.ts
@@ -9,16 +9,17 @@ const VAULT_ADDRESS = '0x05e42c4ae51ba28d8acf8c371009ad7138312ca4'
 export function handleTransfer(event: TransferEvent): void {
   let source = event.params.from
   let destination = event.params.to
-
-  // We only care about the vault
-  if (source.toHex() !== VAULT_ADDRESS && destination.toHex() !== VAULT_ADDRESS) {
-    return
+  let txHash = event.transaction.hash.toHex()
+  let entity: TransferEntity
+  // // We only care about the vault
+  if (source.toHex() == VAULT_ADDRESS || destination.toHex() == VAULT_ADDRESS) {
+    entity = new TransferEntity(txHash + '-' + event.logIndex.toString())
+    entity.amount = event.params.value
+    entity.source = source
+    entity.destination = destination
+    entity.txHash = txHash
+    entity.timestamp = event.block.timestamp
+    entity.save()
   }
-
-  let entity = new TransferEntity(`${event.transaction.hash}-${event.logIndex.toString()}`)
-  entity.amount = event.params.value
-  entity.source = source
-  entity.destination = destination
-  entity.txHash = event.transaction.hash
-  entity.save()
+  return
 }
